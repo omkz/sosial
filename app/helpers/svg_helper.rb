0518fde476319003b76254ace11ebfe7f017bdbe
Kurnia Muhamad <omenkzz@gmail.com>
2024-06-02 00:13:51 +0700

    svg update in Post
    - replace with svg image
    - update svg in view

---
+++ app/helpers/svg_helper.rb
@@ -0,0 +1,11 @@
+module SvgHelper
+  def svg_tag(icon_name, options={})
+    file = File.read(Rails.root.join('app', 'assets', 'images', 'svg', "#{icon_name}.svg"))
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+
+    options.each {|attr, value| svg[attr.to_s] = value}
+
+    doc.to_html.html_safe
+  end
+end
