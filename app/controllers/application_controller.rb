c61ddc32881e2e9fdbdd795a9a444de06bd9609c
Kurnia Muhamad <omenkzz@gmail.com>
2024-06-11 15:16:33 +0700

    Add avatar
    - add avatar to homepage
    - add avatar to comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
-     protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-     before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
 
-     protected
-
-       def configure_permitted_parameters
-         devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
-         devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name])
-       end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :username, :name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :username, :name])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,17 @@ class User < ApplicationRecord
   validates :name, presence: true, numericality: false, length: { minimum: 3 }
 
   has_many :likes, dependent: :destroy
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
+
+  after_commit :add_default_avatar, on: [:create, :update]
+
+  private
+
+  def add_default_avatar
+    unless avatar.attached?
+      self.avatar.attach(io: File.open(Rails.root.join("app", "assets", "images", "avatar.png")), filename: 'avatar.png', content_type: "image/png")
+    end
+  end
 end
